bfb4a3f74ccb20b3ecbff955c10bcc8a2e45c453
tushar <deshmukht354@gmail.com>
2025-09-13 23:04:50 +0530

    Files Updated

--- src/components/ReactNotes.jsx
+++ src/components/ReactNotes.jsx
@@ -1,21 +1,21 @@
-import notesData from '../api/notesData.json';
-import { Footer } from './Footer';
-import { Header } from './Header';
-import { NotesContent } from './NotesContent';
+import notesData from "../api/notesData.json";
+import { Footer } from "./Footer";
+import { Header } from "./Header";
+import { NotesContent } from "./NotesContent";
 
 const ReactNotes = () => {
-    return(
-        <>
-            {/* <Header /> */}
-            <h1>REACT.JS NOTES</h1>
-            <ul>
-                {notesData.map((curElem) =>
-                    <NotesContent key={curElem.id} data = {curElem} />
-                )}
-            </ul>
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      {/* <Header /> */}
+      <h1>REACT.JS NOTES</h1>
+      <ul>
+        {notesData.map((curElem) => (
+          <NotesContent key={curElem.id} data={curElem} />
+        ))}
+      </ul>
+      <Footer />
+    </>
+  );
+};
 
 export default ReactNotes;
